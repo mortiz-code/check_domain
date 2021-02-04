7fe42a4fcab78fd33cb1788588965ef074d889a4
Matias Ortiz <ortiz.matias@yahoo.com.ar>
2021-02-04 09:08:36 -0300

    added verbose mode

--- README.md
+++ README.md
@@ -36,14 +36,14 @@ By default, the app will try to check DKIM with selector 'domain' and if it fail
 Both selectors can be defined manually. For domain use "-d" and set optional flags "-s" for DKIM selector and/or "-b" for BIMI selector. Another way is using the interactive mode.
 
 ```python
-    └─$ python check_domain.py -h
-    usage: check_domain.py [-h] [-d DOMAIN] [-s SELECTOR_DKIM] [-b SELECTOR_BIMI]
-
-    optional arguments:
-    -h, --help        show this help message and exit
-    -d DOMAIN         Domain to be analyzed.
-    -s SELECTOR_DKIM  Selector DKIM.
-    -b SELECTOR_BIMI  Selector BIMI.
+└─$ python check_domain.py -h
+usage: check_domain.py [-h] -d DOMAIN [-s SELECTOR_DKIM] [-b SELECTOR_BIMI] [-v]
+optional arguments:
+-h, --help        show this help message and exit
+-d DOMAIN         Domain to be analyzed.
+-s SELECTOR_DKIM  Selector DKIM.
+-b SELECTOR_BIMI  Selector BIMI.
+-v, --verbose     Verbose mode.
 ```
 
 Option 1:
@@ -52,28 +52,53 @@ Option 1:
 └─$ python check_domain.py
 ==================================================
 Domain to be analyzed: bvstv.com
+Verbose mode? [Y/N]: y
 Do you know DKIM or BIMI selector? [Y/N]: y
 Choose DKIM selector: bvstv
 Choose BIMI selector: default
 ==================================================
 [+] A Record : 190.61.250.160
 [.] Record not found AAAA in domain 'bvstv.com'.
-[+] MX Record : 0 mx1.hc4885-28.iphmx.com.
 [+] MX Record : 0 mx2.hc4885-28.iphmx.com.
-[.] Record not found TXT in domain 'bvstv.com'.
-[+] TXT Record : "v=spf1 ip4:190.61.250.160 ip4:68.232.140.76 ip4:68.232.146.126 ip4:68.232.143.39 ip4:68.232.148.166 ip4:23.90.98.133 ip4:181.166.215.252 include:servers.mcsv.net -all"
+[+] MX Record : 0 mx1.hc4885-28.iphmx.com.
+[+] TXT Record : "v=DKIM1; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA13K6/iYmOCA+KH62zxDWdH+wn1X2ZOllAMZ/KZfvwZWKwhHQGGSU+eHCsyWbz0jJYiS1X/4C6NLya2GXrLAAkxVA8l74aPTN5yBeZP0iXBvZ1Yl
+47VW9C6ElRdlPyAimSiyOta5tDjSamJHGFxvwzJ5y2zh11qOYGQMCq8jEQAFX8+9CWx4t4HQ5uAwHrzXTPS3kNRn2"
+"H/ubJYnZmk943v51rBb2iM9DPymWMObfjIH7rgFJUb5j6+PGwNbFgw42cjbhEBd1YGBh+K4+/PBPSuOSG+miAApD+4Ki3icjt0KaOdrKaGiah+elSgElprDIIADlRHyNxsXPSLrsBXghrQIDAQAB;"
+[+] TXT Record : "duo_sso_verification=ntfsmAmvYMYMnwjgk6SpssPl5t7hZADsv9NCBLtCS7AnylaapsIfsFB9k6PItJVr"
+[+] SPF Record:
+ 1) v=spf1
+ 2) ipv4 -> ['190.61.250.160']
+ 3) ipv4 -> ['68.232.140.76']
+ 4) ipv4 -> ['68.232.146.126']
+ 5) ipv4 -> ['68.232.143.39']
+ 6) ipv4 -> ['68.232.148.166']
+ 7) ipv4 -> ['23.90.98.133']
+ 8) ipv4 -> ['181.166.215.252']
+ 9) servers.mcsv.net -> 'Third-party email service as a trustworthy sender.' Recursive query: "v=spf1 ip4:205.201.128.0/20 ip4:198.2.128.0/18 ip4:148.105.8.0/21 ?all"
+ 10) -all -> 'The SPF record specifies explicitly that nothing can be said about validity.'
 [+] TXT Record : "MS=ms87202626"
 [+] TXT Record : "cisco-ci-domain-verification=56425da1647ff1346e2c9345925928b74fc75b04c218da0f7928711df8fc160b"
-[+] TXT Record : "v=DKIM1; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA13K6/iYmOCA+KH62zxDWdH+wn1X2ZOllAMZ/KZfvwZWKwhHQGGSU+eHCsyWbz0jJYiS1X/4C6NLya2GXrLAAkxVA8l74aPTN5yBeZP0iXBvZ1Yl47VW9C6ElRdlPyAimSiyOta5tDjSamJHGFxvwzJ5y2zh11qOYGQMCq8jEQAFX8+9CWx4t4HQ5uAwHrzXTPS3kNRn2" "H/ubJYnZmk943v51rBb2iM9DPymWMObfjIH7rgFJUb5j6+PGwNbFgw42cjbhEBd1YGBh+K4+/PBPSuOSG+miAApD+4Ki3icjt0KaOdrKaGiah+elSgElprDIIADlRHyNxsXPSLrsBXghrQIDAQAB;"
-[+] TXT Record : "duo_sso_verification=ntfsmAmvYMYMnwjgk6SpssPl5t7hZADsv9NCBLtCS7AnylaapsIfsFB9k6PItJVr"
-[+] DKIM Record : "v=DKIM1; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA13K6/iYmOCA+KH62zxDWdH+wn1X2ZOllAMZ/KZfvwZWKwhHQGGSU+eHCsyWbz0jJYiS1X/4C6NLya2GXrLAAkxVA8l74aPTN5yBeZP0iXBvZ1Yl47VW9C6ElRdlPyAimSiyOta5tDjSamJHGFxvwzJ5y2zh11qOYGQMCq8jEQAFX8+9CWx4t4HQ5uAwHrzXTPS3kNRn2" "H/ubJYnZmk943v51rBb2iM9DPymWMObfjIH7rgFJUb5j6+PGwNbFgw42cjbhEBd1YGBh+K4+/PBPSuOSG+miAApD+4Ki3icjt0KaOdrKaGiah+elSgElprDIIADlRHyNxsXPSLrsBXghrQIDAQAB;"
+[+] DKIM Record : "v=DKIM1; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA13K6/iYmOCA+KH62zxDWdH+wn1X2ZOllAMZ/KZfvwZWKwhHQGGSU+eHCsyWbz0jJYiS1X/4C6NLya2GXrLAAkxVA8l74aPTN5yBeZP0iXBvZ1Y
+l47VW9C6ElRdlPyAimSiyOta5tDjSamJHGFxvwzJ5y2zh11qOYGQMCq8jEQAFX8+9CWx4t4HQ5uAwHrzXTPS3kNRn2"
+"H/ubJYnZmk943v51rBb2iM9DPymWMObfjIH7rgFJUb5j6+PGwNbFgw42cjbhEBd1YGBh+K4+/PBPSuOSG+miAApD+4Ki3icjt0KaOdrKaGiah+elSgElprDIIADlRHyNxsXPSLrsBXghrQIDAQAB;"
+[+] DMARC Record :
+ 1) v=DMARC1;
+ 2) p=reject; -> 'Policy for organizational domain.'
+ 3) adkim=r; -> 'Alignment mode for DKIM.'
+ 4) aspf=r; -> 'Alignment mode for SPF.'
+ 5) fo=0;
+ 6) pct=100; -> 'Percentage of messages subjected to filtering.'
+ 7) rf=afrf;
+ 8) ri=86400;
+ 9) rua=mailto:bvstv@rua.dmp.cisco.com,mailto:netsec@bvstv.com; -> 'Reporting URI of aggregate reports.'
+ 10) ruf=mailto:bvstv@ruf.dmp.cisco.com,mailto:netsec@bvstv.com -> 'Reporting URI for forensic reports.'
 [+] BIMI Record : "v=BIMI1; l=https://bvstv.com/img/bvs_bimi.svg; a=self;"
 ```
 
 Option 2:
 
 ```python
-└─$ python check_domain.py -d cisco.com
+└─$ python check_domain.py -d cisco.com -v
 ==================================================
 [+] A Record : 72.163.4.185
 [+] AAAA Record : 2001:420:1101:1::185
@@ -82,7 +107,10 @@ Option 2:
 [+] MX Record : 30 aer-mx-01.cisco.com.
 [+] TXT Record : "MS=ms35724259"
 [+] TXT Record : "926723159-3188410"
-[+] TXT Record : "v=spf1 redirect=spfa._spf.cisco.com"
+[+] SPF Record:
+ 1) v=spf1
+ 2) spfa._spf.cisco.com -> 'Replaces domain with the current record.' Recursive query: "v=spf1 ip4:173.37.147.224/27 ip4:173.37.142.64/26 ip4:173.38.212.128/27 ip4:173.38.203.0/24 ip4:72.163.7.160/27 ip4:72.163.197.0/24 ip4:66.187.208.0/20 ip4:173.37.86.0/24
+include:spf.protection.outlook.com include:spfb._spf.cisco.com ~all"
 [+] TXT Record : "apple-domain-verification=qOInipPgso3W8cmK"
 [+] TXT Record : "QuoVadis=94d4ae74-ecd5-4a33-975e-a0d7f546c801"
 [+] TXT Record : "SFMC-o7HX74BQ79k7glpt_qjlF2vmZO9DpqLtYxKLwg87"
@@ -97,6 +125,7 @@ Option 2:
 [+] TXT Record : "onetrust-domain-verification=20345dd0c33946f299f14c1498b41f67"
 [+] TXT Record : "identrust_validate=JnSSfW+y58dEQju6mVBe8lu1MGFepXI50P27OE1ZZQmL"
 [+] TXT Record : "identrust_validate=Wns4/AOM0Ij2kQCQhzvNbMcoBzxItOa+44O7KF06lIp3"
+[+] TXT Record : "fastly-domain-delegation-e9a758d22183504af2d5ab4d9a9853da-20210127"
 [+] TXT Record : "google-site-verification=9MlQU9MMQ1jHLMUkONKe6QzZ-ZIGRv0BCD1_rY1Zdmc"
 [+] TXT Record : "google-site-verification=lW5eqPMJI4VrLc28YW-JBkqA-FDNVnhFCXQVDvFqZTo"
 [+] TXT Record : "google-site-verification=qPS9ZkoQ-Og1rBrM1_N7z-tNJNy2BVxE8lw6SB2iFdk"
@@ -104,6 +133,14 @@ Option 2:
 [+] TXT Record : "zpSH7Ye/seyY61hH8+Rq5Kb+ZJ9hDa+qeFBaD/6sPAAg+2POkGdP0byHb1pFVK9uZgYF2AIosUSZq4MB17oydQ=="
 [+] TXT Record : "atlassian-domain-verification=672RcADvt8BPqsb9gCN2ZC5DoTAhUT8abC1blYKQxi/MHMaGoA/BuvjFMaWRtgd7"
 [.] DKIM Record : Selector 'cisco' or 'selector1' is not found in the DNS records. Check DKIM configuration or choose the manual selector option.
+[+] DMARC Record :
+ 1) v=DMARC1;
+ 2) p=quarantine; -> 'Policy for organizational domain.'
+ 3) pct=0; -> 'Percentage of messages subjected to filtering.'
+ 4) fo=1;
+ 5) ri=3600;
+ 6) rua=mailto:cisco@rua.agari.com; -> 'Reporting URI of aggregate reports.'
+ 7) ruf=mailto:cisco@ruf.agari.com -> 'Reporting URI for forensic reports.'
 [.] BIMI Record : Policy not is found in DNS record using selector 'default'. Check BIMI configuration or choose the manual selector option.
 ```
 
--- check_domain.py
+++ check_domain.py
@@ -1,12 +1,15 @@
 """
 Autor: Matias Ortiz.
 Contact: matias.ortiz@bvstv.com
-Date: 24/01/2021
+Date: 4/02/2021
+Version 2
 """
 
 import dns.resolver
 from argparse import ArgumentParser
 from sys import exit, argv
+from rich import print
+import re
 
 
 def main():
@@ -15,17 +18,23 @@ def main():
     """
     # Construct an argument parser
     all_args = ArgumentParser()
-    all_args.add_argument("-d", dest="Domain", help="Domain to be analyzed.")
+    all_args.add_argument("-d", dest="Domain", help="Domain to be analyzed.", required=True)
     all_args.add_argument("-s", dest="Selector_DKIM", help="Selector DKIM.")
     all_args.add_argument("-b", dest="Selector_BIMI", help="Selector BIMI.")
+    all_args.add_argument("-v", "--verbose", action="count", default=None, help="Verbose mode.")
     args = all_args.parse_args()
     domain = args.Domain
     selector_dkim = args.Selector_DKIM
     selector_bimi = args.Selector_BIMI
-    if selector_dkim and selector_bimi != None:
+    verbose = args.verbose
+    if verbose != None:
+        check_verbose(domain, selector_dkim, selector_bimi)
+    elif selector_dkim and selector_bimi != None:
         check(domain, selector_dkim, selector_bimi)
-    elif selector_dkim != None:
-        check(domain, selector_dkim)
+    elif selector_dkim and verbose != None:
+        check_verbose(domain, selector_dkim)
+    elif selector_bimi != None:
+        check(domain, selector_bimi)
     else:
         check(domain)
 
@@ -36,26 +45,38 @@ def main2():
     """
     print("=" * 50)
     try:
-        domain = input("Domain to be analyzed: ")
+        domain = input("Domain to be analyzed: ").lower()
+        verbose = input("Verbose mode? [Y/N]: ").lower()
         opt = input("Do you know DKIM or BIMI selector? [Y/N]: ").lower()
-        if opt == "y":
+        if verbose in "y" and opt == "y":
+            selector_dkim = input("Choose DKIM selector: ")
+            selector_bimi = input("Choose BIMI selector: ")
+            check_verbose(domain, selector_dkim, selector_bimi)
+
+        elif verbose in "y" and opt != "y":
+            check_verbose(domain)
+
+        elif verbose in "n" and opt in "y":
             selector_dkim = input("Choose DKIM selector: ")
             selector_bimi = input("Choose BIMI selector: ")
             check(domain, selector_dkim, selector_bimi)
+
         else:
             check(domain)
-    except:
-        print("=" * 50, "Closing app...", sep="\n")
-        exit()
+
+    except KeyboardInterrupt as e:
+        print("\n", "=" * 50, "Closing app...", sep="\n")
+        exit(e)
 
 
 def check(domain, selector_dkim=None, selector_bimi=None):
     """
-    check Check DNS Record
+    Check DNS Record.
 
     Args:
         domain (str): Domain to be analyzed.
         manual_selector (str, optional): Selector for DKIM checks. Defaults to None.
+        selector_bimi (str, optional): Selector for BIMI checks. Defaults to None.
     """
     print("=" * 50)
 
@@ -67,13 +88,17 @@ def check(domain, selector_dkim=None, selector_bimi=None):
         "MX",
         "TXT",
     ]
-    for i in type:
+
+    for record in type:
         try:
-            result = dns.resolver.resolve(domain, i)
+            result = dns.resolver.resolve(domain, record)
             for val in result:
-                print(f"[+] {i} Record :", val.to_text())
+                if "spf" in val.to_text():
+                    print("[+] SPF Record: ", val.to_text())
+                else:
+                    print(f"[+] {record} Record :", val.to_text())
         except:
-            print(f"[.] Record not found {i} in domain '{domain}'.")
+            print(f"[.] Record not found {record} in domain '{domain}'.")
 
     # Printing record
     if selector_dkim != None:
@@ -99,6 +124,198 @@ def check(domain, selector_dkim=None, selector_bimi=None):
                 print(
                     f"[.] DKIM Record : Selector '{selector[0]}' or 'selector1' is not found in the DNS records. Check DKIM configuration or choose the manual selector option."
                 )
+
+    # Printing record
+    try:
+        result = dns.resolver.resolve(f"_dmarc.{domain}", "TXT")
+        for val in result:
+            print(f"[+] DMARC Record :", val.to_text())
+
+    except:
+        print(f"[.] DMARC : Record not found in DNS record. Check DKIM configuration.")
+
+    # Printing record
+    if selector_bimi != None:
+        try:
+            result = dns.resolver.resolve(f"{selector_bimi}._bimi.{domain}", "TXT")
+            for val in result:
+                print("[+] BIMI Record :", val.to_text())
+        except:
+            print(
+                f"[.] BIMI Record : Policy not found in DNS record using selector '{selector_bimi}'. Check BIMI configuration."
+            )
+    else:
+        try:
+            result = dns.resolver.resolve(f"default._bimi.{domain}", "TXT")
+            for val in result:
+                print("[+] BIMI Record :", val.to_text())
+        except:
+            print(
+                f"[.] BIMI Record : Policy not is found in DNS record using selector 'default'. Check BIMI configuration or choose the manual selector option."
+            )
+
+
+def check_verbose(domain, selector_dkim=None, selector_bimi=None):
+    """
+    Check DNS Record verbose mode.
+
+    Args:
+        domain (str): Domain to be analyzed.
+        manual_selector (str, optional): Selector for DKIM checks. Defaults to None.
+        selector_bimi (str, optional): Selector for BIMI checks. Defaults to None.
+    """
+    print("=" * 50)
+
+    selector = domain.split(".")
+
+    type = [
+        "A",
+        "AAAA",
+        "MX",
+        "TXT",
+    ]
+
+    count = 0
+
+    reg_ip = r"(?:\d{1,3}\.)+(?:\d{1,3})"
+    reg_mask = r"(?:\d{1,3}\.)+(?:\d{1,3})['/']\d{1,2}"
+
+    for record in type:
+        try:
+            result = dns.resolver.resolve(domain, record)
+            for val in result:
+                if "spf" in val.to_text():
+                    records = val.to_text().split(" ")
+                    print("[+] SPF Record: ")
+                    for rec in range(len(records)):
+                        count += 1
+                        j = records[rec]
+                        if "v=" in j:
+                            j = j.replace('"', "")
+                            print(f" {count}) {j}")
+                        elif "ip4:" in records[rec]:
+                            if re.search("/", records[rec]):
+                                i = re.findall(reg_mask, records[rec])
+                                print(f" {count}) ipv4 -> {i}")
+                            else:
+                                i = re.findall(reg_ip, records[rec])
+                                print(f" {count}) ipv4 -> {i}")
+                        elif "include:" in j:
+                            j = j.replace('"', "")
+                            ips = j.split("include:")
+                            print(
+                                f" {count}) {ips[1]} -> 'Third-party email service as a trustworthy sender.' Recursive query: ",
+                                end="",
+                            )
+                            try:
+                                result = dns.resolver.resolve(ips[1], "TXT")
+                                for val in result:
+                                    print(val.to_text())
+                            except:
+                                print(f"[.] Record not found {ips[1]} in domain '{domain}'.")
+                        elif "redirect=" in j:
+                            j = j.replace('"', "")
+                            ips = j.split("redirect=")
+                            print(
+                                f" {count}) {ips[1]} -> 'Replaces domain with the current record.' Recursive query: ",
+                                end="",
+                            )
+                            try:
+                                result = dns.resolver.resolve(ips[1], "TXT")
+                                for val in result:
+                                    print(val.to_text())
+                            except:
+                                print(f"[.] Record not found {ips[1]} in domain '{domain}'.")
+                        elif "all" in j:
+                            if "+all" in val.to_text():
+                                j = j.replace('"', "")
+                                print(
+                                    f" {count}) {j} -> 'The SPF record designates the host to be allowed to send.'"
+                                )
+                            elif "-all" in val.to_text():
+                                j = j.replace('"', "")
+                                print(
+                                    f" {count}) {j} -> 'The SPF record has designated the host as NOT being allowed to send.'"
+                                )
+                            elif "~all" in val.to_text():
+                                j = j.replace('"', "")
+                                print(
+                                    f" {count}) {j} -> 'The SPF record has designated the host as NOT being allowed to send but is in transition.'"
+                                )
+                            elif "?all" in val.to_text():
+                                j = j.replace('"', "")
+                                print(
+                                    f" {count}) {j} -> 'The SPF record specifies explicitly that nothing can be said about validity.'"
+                                )
+                            else:
+                                j = j.replace('"', "")
+                                print(
+                                    f" {count}) The domain '{domain}' does not have an SPF record or the SPF record does not evaluate to a result."
+                                )
+
+                        else:
+                            print(f" {count}) {j}")
+
+                else:
+                    print(f"[+] {record} Record :", val.to_text())
+        except:
+            print(f"[.] Record not found {record} in domain '{domain}'.")
+
+    # Printing record
+    if selector_dkim != None:
+        try:
+            result = dns.resolver.resolve(f"{selector_dkim}._domainkey.{domain}", "TXT")
+            for val in result:
+                print("[+] DKIM Record :", val.to_text())
+        except:
+            print(
+                f"[.] DKIM Record : Selector '{selector_dkim}' not found in DNS record. Check DKIM configuration."
+            )
+    else:
+        try:
+            result = dns.resolver.resolve(f"{selector[0]}._domainkey.{domain}", "TXT")
+            for val in result:
+                print("[+] DKIM Record :", val.to_text())
+        except:
+            try:
+                result = dns.resolver.resolve(f"selector1._domainkey.{domain}", "TXT")
+                for val in result:
+                    print("[+] DKIM Record :", val.to_text())
+            except:
+                print(
+                    f"[.] DKIM Record : Selector '{selector[0]}' or 'selector1' is not found in the DNS records. Check DKIM configuration or choose the manual selector option."
+                )
+
+    # Printing record
+    try:
+        result = dns.resolver.resolve(f"_dmarc.{domain}", "TXT")
+        count = 0
+        for val in result:
+            print("[+] DMARC Record :")
+            records = val.to_text().split(" ")
+            for record in records:
+                count += 1
+                record = record.replace('"', "")
+                record = record.replace(" ", "")
+                if "p=" in record:
+                    print(f" {count}) {record} -> 'Policy for organizational domain.'")
+                elif "adkim=" in record:
+                    print(f" {count}) {record} -> 'Alignment mode for DKIM.'")
+                elif "aspf=" in record:
+                    print(f" {count}) {record} -> 'Alignment mode for SPF.'")
+                elif "pct=" in record:
+                    print(f" {count}) {record} -> 'Percentage of messages subjected to filtering.'")
+                elif "rua=" in record:
+                    print(f" {count}) {record} -> 'Reporting URI of aggregate reports.'")
+                elif "ruf=" in record:
+                    print(f" {count}) {record} -> 'Reporting URI for forensic reports.'")
+                elif "sp=" in record:
+                    print(f" {count}) {record} -> 'Policy for subdomains of the OD.'")
+                else:
+                    print(f" {count}) {record}")
+
+    except:
+        print(f"[.] DMARC : Record not found in DNS record. Check DKIM configuration.")
 
     # Printing record
     if selector_bimi != None:
